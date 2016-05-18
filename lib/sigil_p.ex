dd20582fc105a015c286a373f9f4a8d687b74e63
Kabie <kabie3000@gmail.com>
2016-05-19 05:26:13 +0800

    draft test

--- lib/sigil_p.ex
+++ lib/sigil_p.ex
@@ -1,2 +1,13 @@
 defmodule SigilP do
+
+  defmacro __using__(_opts) do
+    quote do
+      import SigilP
+    end
+  end
+
+  defmacro sigil_p(string, modifiers) do
+    string
+  end
+
 end
--- test/sigil_p_test.exs
+++ test/sigil_p_test.exs
@@ -2,7 +2,24 @@ defmodule SigilPTest do
   use ExUnit.Case
   doctest SigilP
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  use SigilP
+
+  test "hex" do
+    assert ~p"12 07 74 65 73 74 69 6e 67"x ==
+          <<0x12, 0x07, 0x74, 0x65, 0x73, 0x74, 0x69, 0x6e, 0x67>>
+  end
+
+  test "bin" do
+    assert ~p"1001 0110  0000 0001"b ==
+          <<0x96, 0x01>>
+  end
+
+  test "base64" do
+  end
+
+  test "url" do
+  end
+
+  test "xml" do
   end
 end
